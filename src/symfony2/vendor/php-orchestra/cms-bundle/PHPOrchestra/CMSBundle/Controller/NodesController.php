2088be29269cf929b0c096366c02b9fc0cabcebb
Noël Gilain <noel.gilain@businessdecision.com>
2014-03-11 18:07:07 +0100

    Enhanced block rendering to display all customs attributes

--- src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Controller/NodesController.php
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Controller/NodesController.php
@@ -17,37 +17,37 @@ class NodesController extends Controller
 // Block #1 : Site Menu
         $block1 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'Site Menu'));
+            ->setAttributes(array('rubrique A' => 'Qui sommes-nous ?', 'rubrique B' => 'pourquoi nous choisir ?', 'rubrique C' => 'Nos agences'));
         
 // Block #2 : Left Menu
         $block2 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'Left Menu'));
+            ->setAttributes(array('lien 1' => 'http://www.google.fr', 'lien 2' => 'http://www.yahoo.fr', 'lien 3' => 'http://altavista.box.co.uk'));
         
 // Block #3 : News #1
         $block3 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'News 1'));
+            ->setAttributes(array('title' => 'News 1', 'content' => 'Donec bibendum at nibh eget imperdiet. Mauris eget justo augue. Fusce fermentum iaculis erat, sollicitudin elementum enim sodales eu. Donec a ante tortor. Suspendisse a.'));
         
 // Block #4 : News #2
         $block4 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'News #2'));
+            ->setAttributes(array('title' => 'News #2', 'content' => 'Aliquam convallis facilisis nulla, id ultricies ipsum cursus eu. Proin augue quam, iaculis id nisi ac, rutrum blandit leo. In leo ante, scelerisque tempus lacinia in, sollicitudin quis justo. Vestibulum.'));
         
 // Block #5 : News #3
         $block5 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'News #3'));
+            ->setAttributes(array('content' => 'News #3', 'content' => 'Phasellus condimentum diam placerat varius iaculis. Aenean dictum, libero in sollicitudin hendrerit, nulla mi elementum massa, eget mattis lorem enim vel magna. Fusce suscipit orci vitae vestibulum.'));
         
 // Block #6 : Pub
         $block6 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'Pub'));
+            ->setAttributes(array('image' => 'mapub.jpg'));
 
 // Block #7 : Legal mentions
         $block7 = $mandango->create('Model\PHPOrchestraCMSBundle\Block')
             ->setComponent('PHPOrchestraCMSBundle:Block:show')  
-            ->setAttributes(array('content' => 'Legal mentions'));
+            ->setAttributes(array('Lien A' => 'Mentions légales', 'Lien B' => 'Nous contacter'));
             
 // Area 1 : Header
         $area1 = new area();
