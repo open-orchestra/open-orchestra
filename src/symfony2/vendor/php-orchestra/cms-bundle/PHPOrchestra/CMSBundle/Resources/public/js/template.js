3d9bc8be9b41863610e3269c4019e1b75ba71fd8
Nicolas Anne <nanne@nane>
2014-03-20 15:08:09 +0100

    (change div to li + add droppable) for template generator

--- src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Resources/public/js/template.js
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Resources/public/js/template.js
@@ -1,49 +1,139 @@
 (function($){
+	$.generateFunction = function(type){
+		var functions = {'create': function(){return false;},
+				'add': function(){return false;},
+				'save': function(){return false;},
+				'open': function(){return false;},
+				'send': function(){return false;}
+				}
+		functions['save'] = function(obj, settings, this_values){
+			obj.find(":input").each(function(){
+				var name = $(this).attr('name');
+				this_values[name] = $(this).val();
+			});
+		}
+		functions['open'] = function(obj, settings, this_values){
+			obj.find(":input").each(function(){
+				var name = $(this).attr('name');
+				$(this).val(this_values[name]);
+			});
+		}
+		if(type == "node"){
+			functions = $.extend(functions, $.generateNodeFunction());
+		}
+		else if(type == "areas" || type == "subAreas"){
+			functions = $.extend(functions, $.generateAreaFunction());
+		}
+		else if(type == "blocks"){
+			functions = $.extend(functions, $.generateBlockFunction());
+		}
+		return functions;
+	}
+	$.generateNodeFunction = function(){
+		var empty_area = {"classes": [], "subAreas": [], "blocks": []};
+		var create = function(obj, settings, this_values){
+			var buttons = obj.dialog( "option", "buttons" );
+			delete buttons['Add Blocks'];
+			delete buttons['Add Areas'];
+			obj.dialog( "option", "buttons", buttons);
+		}
+		var add = function(obj, settings, this_values){
+			this_values.direction = obj.find("select[name=\'direction\']").val();
+			this_values.areas.push(empty_area);
+		}
+		var send = function(obj, settings, this_values){
+			$("#template_siteId").val(settings.values.siteId);
+			$("#template_name").val(settings.values.name);
+			$("#template_version").val(settings.values.version);
+			$("#template_language").val(settings.values.language);
+			$("#template_status").val(settings.values.status);
+			$("#template_templateId").val(settings.values.templateId);
+			$("#template_areas").val(JSON.stringify(settings.values.areas));
+			$("#template_blocks").val(JSON.stringify(settings.values.blocks));
+			$("form[name='template']").submit();
+		}
+		return {'create': create,
+				'add': add,
+				'send': send
+			};
+	}
+	$.generateAreaFunction = function(settings, this_values){
+		var empty_area = {"classes": [], "subAreas": [], "blocks": []};
+		var empty_block = {"component": "No Controller", "attributes": []};
+		var create = function(obj, settings, this_values){
+			var buttons = obj.dialog( "option", "buttons" );
+			delete buttons['Send'];
+			if(this_values.blocks.length > 0 || this_values.subAreas.length > 0){
+				delete buttons['Add Blocks'];
+				delete buttons['Add Areas'];
+			}
+			else{
+				delete buttons['Add'];
+			}
+			obj.dialog( "option", "buttons", buttons);
+		}
+		var add = function(obj, settings, this_values, type){
+			this_values.direction = obj.find("select[name=\'direction\']").val();
+			if(this_values.blocks.length > 0 || type == 'block'){
+				var length = settings.values.blocks.length;
+				settings.values.blocks[length] = empty_block;
+				this_values.blocks.push({"nodeId": 0, "blockId": length});
+			}
+			else{
+				this_values.subAreas.push(empty_area);
+			}
+		}
+		return {'create': create,
+			'add': add
+		};
+	}
+	$.generateBlockFunction = function(settings, this_values){
+		var create = function(obj, settings, this_values){
+			var buttons = obj.dialog( "option", "buttons" );
+			delete buttons['Send'];
+			delete buttons['Add'];
+			delete buttons['Add Blocks'];
+			delete buttons['Add Areas'];
+			obj.dialog( "option", "buttons", buttons);
+		}
+
+		var save = function(obj, settings, this_values){
+			settings.values.blocks[this_values.blockId].component = obj.find("input[name=\'component\']").val();
+			settings.values.blocks[this_values.blockId].attributes = obj.find("input[name=\'customAttributes\']").val().split(',');
+		}
+		var open = function(obj, settings, this_values){
+			obj.find("input[name='component']").val(settings.values.blocks[this_values.blockId].component);
+			obj.find("input[name='customAttributes']").val(settings.values.blocks[this_values.blockId].attributes.join(','));
+		}
+		return {'create': create,
+			'save': save,
+			'open': open
+			};
+	}
 	$.createSubTemplate = function(settings, type, target){
 		var values = eval('settings.' + settings.path);
 		var tab = eval('settings.' + settings.path + '.' + type);
 		var path = settings.path + '.' + type;
 		if(tab.length > 0){
-			var width = (values.direction == 'h') ? 100 : (100 / Math.max(tab.length, 1)).toFixed(3);
-			var height = (values.direction == 'v') ? 100 : (100 / Math.max(tab.length, 1)).toFixed(3);
-			var style = {"top": "16px", "left": "16px", "bottom": "16px", "right": "16px"};
 			for(var i in tab){
-				i = parseInt(i);
-				var value0 = (16 * (tab.length - i) / tab.length).toFixed(0);
-				var value1 = (16 * (i + 1) / tab.length).toFixed(0);
-				if(values.direction == 'h'){
-					style.top = value0 + 'px';
-					style.bottom = value1 + 'px';
-				}
-				else if(values.direction == 'v'){
-					style.left = value0 + 'px';
-					style.right = value1 + 'px';
-				}
-				$(target).parseTemplate({"width": width,
-									"height": height,
-									"values": settings.values,
+				$(target).parseTemplate({"values": settings.values,
 									"path": path + '[' + i + ']',
 									"css": settings.css,
-									"style" : style,
+									"style" : {"display": (values.direction == 'v') ? "inline-block" : "block", "width": (values.direction == 'v') ? 100 / (tab.length) + '%' : '100%', "height": (values.direction == 'h') ? 100 / (tab.length) + '%' : '100%'},
 									"index": settings.index + 1,
 									"type": type,
-									"element": settings.element});
+									"element": settings.element}).appendTo(target);
 			}
 		}
 	}
 	$.fn.parseTemplate = function(options)
 	{
 		var empty_node = {"templateId": 0, "siteId": 0, "version": 1, "language": "fr", "status": "draft", "blocks": [], "areas": []};
-		var empty_area = {"classes": [], "subAreas": [], "blocks": []};
-		var empty_block = {"component": "No Controller", "attributes": []};
 		var settings = $.extend({
-			"width": 100,
-			"height": 100,
 			"values": empty_node,
 			"path": null,
 			"css": "ui-widget-model",
 			"style": {"top": "0px", "left": "0px", "bottom": "0px", "right": "0px"},
-			"index": 0,
 			"type": "node",
 			"element": null
 		}, options || {});
@@ -52,22 +142,6 @@
 			settings.element = this;
 			settings.path = "values";
 			settings.element.html('');
-			var html = '';
-
-			html = '<label for="siteId">Site id : </label><input type="text" name="siteId" id="siteId" value=""><br />';
-			html += '<label for="name">Name : </label><input type="text" name="name" id="name" value=""><br />';
-			html += '<label for="language">Language : </label><input type="text" name="language" id="language" value="">';
-			settings.element.append('<div class="dialog-node" style="display:none;" title="Node">' + html + '</div>');
-			
-			html = '<label for="areaId">Area id : </label><input type="text" name="areaId" id="areaId" value=""><br />';
-			html += '<label for="classes">Name : </label><input type="text" name="classes" id="classes" value=""><br />';
-			html += '<label for="addBlocks">add Blocks : </label><input type="checkbox" name="addBlocks" id="addBlocks">';
-			settings.element.append('<div class="dialog-areas dialog-subAreas" style="display:none;" title="Area">' + html + '</div>');
-			
-			html = '<label for="component">Component : </label><input type="text" name="component" id="component" value=""><br />';
-			html += '<label for="customAttribute">Custom Attributes : </label><input type="text" name="customAttributes" id="customAttributes" value=""><br />';
-			settings.element.append('<div class="dialog-blocks" style="display:none;" title="Block">' + html + '</div>');
-
 		}
 		
 		var this_values = eval('settings.' + settings.path);
@@ -81,174 +155,139 @@
 		else if(settings.type == "blocks"){
 			title = ((settings.values.blocks[this_values.blockId].component) ? settings.values.blocks[this_values.blockId].component : 'No Controller');
 		}
-		var legend = $( "<h1/>", {"class": settings.css + '-legend', "text": title});		
-		var fieldset = $( "<div/>", {"class": settings.css + '-fieldset', "css" : settings.style});
-		var div = $( "<div/>", {"class": settings.css + ' ' + settings.css + '-' + settings.type,
-								"css": {"width": String(settings.width) + '%',
-										"height": String(settings.height) + '%',
-										"z-index": settings.index
-										}});
-		legend.appendTo(fieldset);
-		fieldset.appendTo(div);
+		var span = $( "<span/>", {"class": settings.css, "text": title});
+		var div = $( "<div/>", {"class": settings.css});
+		var li = $( "<li/>", {"class": settings.css, "css": settings.style});
+		var ul = $( "<ul/>", {"class": settings.css});
+		span.appendTo(div);
+		div.appendTo(li);
 
-		fieldset.mouseover(function(event){
-			$(this).eq(0).addClass('over');
+		div.mouseover(function(event){
+			$(this).addClass('over');
 			event.stopPropagation();
 		});
-		fieldset.mouseout(function(){
-			$(this).eq(0).removeClass('over');
+		div.mouseout(function(){
+			$(this).removeClass('over');
 		});
-		
-		fieldset.click(function(event){
-			var this_values = eval('settings.' + settings.path);
-			var refresh = function(){return false;};
-			var save = function(){return false;};
-			var open = function(){return false;};
-			var create = function(){return false;};
-			var send = function(){return false;};
-			if(settings.type == "node" || settings.type == "areas" || settings.type == "subAreas"){
-				create = function(obj){
-					var buttons = obj.dialog( "option", "buttons" );
-					if(this_values.direction){
-						delete buttons['Split horizontally'];
-						delete buttons['Split vertically'];
-					}
-					else{
-						delete buttons['Split'];
-					}
-					if(settings.type != "node"){
-						delete buttons['Send'];
-					}
-					obj.dialog( "option", "buttons", buttons);
-				}
+		div.click(function(event){
+			var type = settings.type;
+			if(settings.type == "subAreas"){
+				type = "areas";
 			}
-			else if(settings.type == "blocks"){
-				create = function(obj){
-					var buttons = obj.dialog( "option", "buttons" );
-					delete buttons['Split horizontally'];
-					delete buttons['Split vertically'];
-					delete buttons['Split'];
-					delete buttons['Send'];
-					obj.dialog( "option", "buttons", buttons);
-				}
-			}
-			if(settings.type == "node"){
-				refresh = function(obj){
-					this_values.areas.push(empty_area);
-				}
-				save = function(obj){
-					this_values.siteId = obj.find("input[name=\'siteId\']").val();
-					this_values.name = obj.find("input[name=\'name\']").val();
-					this_values.language = obj.find("input[name=\'language\']").val();
-				}
-				open = function(obj){
-					obj.find("input[name='siteId']").val(this_values.siteId);
-					obj.find("input[name='name']").val(this_values.name);
-					obj.find("input[name='language']").val(this_values.language);
-				}
-				send = function(obj){
-					$("#template_siteId").val(settings.values.siteId);
-					$("#template_name").val(settings.values.name);
-					$("#template_version").val(settings.values.version);
-					$("#template_language").val(settings.values.language);
-					$("#template_status").val(settings.values.status);
-					$("#template_templateId").val(settings.values.templateId);
-					$("#template_areas").val(JSON.stringify(settings.values.areas));
-					$("#template_blocks").val(JSON.stringify(settings.values.blocks));
-					$("form[name='template']").submit();
-				}
-			}
-			else if(settings.type == "areas" || settings.type == "subAreas"){
-				refresh = function(obj){
-					if(obj.find("input[name='addBlocks']").is(':checked')){
-						var length = settings.values.blocks.length;
-						settings.values.blocks[length] = empty_block;
-						this_values.blocks.push({"nodeId": 0, "blockId": length});
-					}
-					else{
-						this_values.subAreas.push(empty_area);
-					}
-				}
-				save = function(obj){
-					this_values.areaId = obj.find("input[name=\'areaId\']").val();
-					this_values.classes = obj.find("input[name=\'classes\']").val().split(',');
-				}
-				open = function(obj){
-					obj.find("input[name='areaId']").val(this_values.areaId);
-					obj.find("input[name='classes']").val(this_values.classes.join(','));
-					if(this_values.blocks.length > 0){
-						obj.find("input[name='addBlocks']").attr("checked", "checked");
-					}
-					if(this_values.blocks.length > 0 || this_values.subAreas.length > 0){
-						obj.find("input[name='addBlocks']").attr('disabled','disabled');;
-					}
-				}
-			}
-			else if(settings.type == "blocks"){
-				save = function(obj){
-					settings.values.blocks[this_values.blockId].component = obj.find("input[name=\'component\']").val();
-					settings.values.blocks[this_values.blockId].attributes = obj.find("input[name=\'customAttributes\']").val().split(',');
-				}
-				open = function(obj){
-					obj.find("input[name='component']").val(settings.values.blocks[this_values.blockId].component);
-					obj.find("input[name='customAttributes']").val(settings.values.blocks[this_values.blockId].attributes.join(','));
-				}
-			}
-			$( ".dialog-" + settings.type ).dialog({
+			var functions = $.generateFunction(type);
+			$( ".dialog-" + type ).dialog({
 				resizable: false,
-				width:490,
+				width:530,
 				modal: true,
 				open: function ( event, ui) {
-					open($(this));
+					functions.open($(this), settings, this_values);
 				},
 				create: function ( event, ui) {
-					create($(this));
+					functions.create($(this), settings, this_values);
 				},
 				buttons: {
 					"Save": function() {
-						save($(this));
+						functions.save($(this), settings, this_values);
 						$(this).dialog( "close" );
 					},
 					"Send": function() {
-						send($(this));
+						functions.send($(this), settings, this_values);
 						$(this).dialog( "close" );
 					},
-					"Split horizontally": function() {
-						this_values.direction = "h";
-						refresh($(this));
+					"Add": function() {
+						functions.add($(this), settings, this_values);
 						$(this).dialog( "close" );
 					},
-					"Split vertically": function() {
-						this_values.direction = "v";
-						refresh($(this));
+					"Add Blocks": function() {
+						functions.add($(this), settings, this_values, 'block');
 						$(this).dialog( "close" );
 					},
-					"Split": function() {
-						refresh($(this));
+					"Add Areas": function() {
+						functions.add($(this), settings, this_values, 'area');
 						$(this).dialog( "close" );
 					}
 				},
 				close: function ( event, ui) {
-					//console.log(JSON.stringify(settings.values.areas));
 					$(this).dialog( "destroy" );
 					settings.element.parseTemplate({'values': settings.values});
 				}
 			});
 			event.stopPropagation();
 		});
-		$(div).draggable();
-		
 		if(this_values.areas){
-			$.createSubTemplate(settings, 'areas', fieldset);
+			ul.addClass(settings.css + '-' + 'areas');
+			$.createSubTemplate(settings, 'areas', ul);
+			ul.children().addClass(settings.css + '-' + 'areas');
+		}
+		else if(this_values.blocks && this_values.blocks.length > 0){
+			ul.addClass(settings.css + '-' + 'blocks');
+			$.createSubTemplate(settings, 'blocks', ul);
+			ul.children().addClass(settings.css + '-' + 'blocks');
+		}
+		else if(this_values.subAreas && this_values.subAreas.length > 0){
+			ul.addClass(settings.css + '-' + 'areas');
+			$.createSubTemplate(settings, 'subAreas', ul);
+			ul.children().addClass(settings.css + '-' + 'areas');
 		}
 		else{
-			if(this_values.blocks && this_values.blocks.length > 0){
-				$.createSubTemplate(settings, 'blocks', fieldset);
-			}
-			else if(this_values.subAreas && this_values.subAreas.length > 0){
-				$.createSubTemplate(settings, 'subAreas', fieldset);
-			}
+			ul.addClass(settings.css + '-' + 'none');
 		}
-		div.appendTo(this);
+		if(settings.type != 'blocks'){
+			ul.appendTo(div);
+		}
+		ul.data('path', settings.path);
+		if(settings.path == "values"){
+			ul = $( "<ul/>", {"class": settings.css + ' ' + settings.css + '-' + 'nodes',
+				"css": {"display": settings.style}});
+			li.appendTo(ul);
+			ul.appendTo(this);
+			$('ul.' + settings.css + '-' + 'areas, ul.' + settings.css + '-' + 'blocks, ul.' + settings.css + '-' + 'none').parent().droppable({
+				greedy: true,
+				tolerance: "pointer",
+				hoverClass: 'over',
+				drop : function(event, ui){
+					var this_values = eval('settings.' + $(this).find('ul').data('path'));
+					var move = eval('settings.' + ui.draggable.data('path'));
+					if("blockId" in move){
+						this_values.blocks.push(move);
+					}
+					else{
+						if("subAreas" in this_values){
+							this_values.subAreas.push(move);
+						}
+						else{
+							this_values.areas.push(move);
+						}
+					}
+					var pattern = new RegExp('^(.*)\\[(\\d*)]$');
+					var path = 'settings.' + ui.draggable.data('path');
+					eval(path.replace(pattern, '$1.splice($2, 1);'));
+					settings.element.parseTemplate({'values': settings.values});
+				},
+				deactivate : function(){
+				}
+			});
+			$('ul.' + settings.css + '-' + 'areas').parent().droppable({
+				accept: 'li.' + settings.css + '-' + 'areas',
+			});
+			$('ul.' + settings.css + '-' + 'none').parent().droppable({
+				accept: 'li.' + settings.css + '-' + 'areas, li.' + settings.css + '-' + 'blocks',
+			});
+			$('ul.' + settings.css + '-' + 'blocks').parent().droppable({
+				accept: 'li.' + settings.css + '-' + 'blocks',
+			});
+		}
+		else{
+			li.data('path', settings.path);
+			li.draggable({
+				opacity: 0.5,
+				containment: settings.element,
+				drag: function(event, ui){
+					settings.element.find('div').unbind('mouseover');
+					settings.element.find('div').unbind('mouseout');
+				}
+			});
+		}
+		return li;
 	}
 })(jQuery);
