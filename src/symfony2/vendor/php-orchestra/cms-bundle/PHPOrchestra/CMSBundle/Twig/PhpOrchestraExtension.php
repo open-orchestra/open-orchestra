6aa4986147d90a58bf1e6dc6fc3536e9a43dc311
Noël Gilain <noel.gilain@businessdecision.com>
2014-03-24 16:10:07 +0100

    Modify security level of render_block function

--- src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Twig/PhpOrchestraExtension.php
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Twig/PhpOrchestraExtension.php
@@ -36,7 +36,7 @@ class PhpOrchestraExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('render_block', array($this, 'renderBlock'))
+            new \Twig_SimpleFunction('render_block', array($this, 'renderBlock'), array('is_safe' => array('html')))
         );
     }
     
@@ -50,7 +50,7 @@ class PhpOrchestraExtension extends \Twig_Extension
         if ($this->is_callable($controllerReference->controller))
             return $this->container->get('fragment.handler')->render($controllerReference);
         else
-            return '<span style="background-color:#FF0000">Controller non valide</span><br/>';
+            return false;
     }
 
     /**
@@ -58,13 +58,13 @@ class PhpOrchestraExtension extends \Twig_Extension
      * 
      * @param string $actionLogicalName
      */
-    private function is_callable($actionLogicalName)
+    protected function is_callable($actionLogicalName)
     {
         $status = false;
         list($bundleName, $controllerName, $actionName) = explode(':', $actionLogicalName, 3);
         
         $bundle = $this->container->get('kernel')->getBundle($bundleName);
-        $class = $bundle->getNamespace() . '\\Controller\\'. $controllerName . 'Controller';
+        $class = $bundle->getNamespace() . '\\Controller\\' . $controllerName . 'Controller';
         
         if ((class_exists($class)) && is_callable(array($class, $actionName . 'Action')))
             $status = true;
