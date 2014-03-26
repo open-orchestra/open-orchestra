a24f8e693a21b2aed7e6ad7fd1e2677e2d278027
Noël Gilain <noel.gilain@businessdecision.com>
2014-03-26 17:47:12 +0100

    Inject PHPOrchestraUrlMatcher in symfony router

--- src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/DependencyInjection/PHPOrchestraCMSExtension.php
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/DependencyInjection/PHPOrchestraCMSExtension.php
@@ -23,6 +23,7 @@ class PHPOrchestraCMSExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.yml');
         $loader->load('services.yml');
     }
 }
