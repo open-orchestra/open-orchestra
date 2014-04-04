f0c722c70de909d253c3b76e441cdb3adbd22a87
Nicolas BOUQUET <nicolas.bouquet@businessdecision.com>
2014-04-04 18:23:27 +0200

    Adding a dummy unit test and moving Tests to Test

---
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Test/DummyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPOrchestra\CMSBundle\Test;
+
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
