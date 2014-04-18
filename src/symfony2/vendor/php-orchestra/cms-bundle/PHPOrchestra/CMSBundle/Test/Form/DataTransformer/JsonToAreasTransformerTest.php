c8829b8cb797c746db46fabea57c6f031680f033
Nicolas BOUQUET <nicolas.bouquet@businessdecision.com>
2014-04-18 12:45:49 +0200

    Add a simple unit test for JsonToAreasTransformer

---
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Test/Form/DataTransformer/JsonToAreasTransformerTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace PHPOrchestra\CMSBundle\Test\Form\DataTransformer;
+
+use \PHPOrchestra\CMSBundle\Form\DataTransformer;
+
+/**
+ * Description of JsonToAreasTransformerTest
+ *
+ * @author nbouquet
+ */
+class JsonToAreasTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Transformer object to test
+     * 
+     * @var \PHPOrchestra\CMSBundle\Form\DataTransformer\JsonToAreasTransformer
+     */
+    protected $transformer;
+    
+    /**
+     * Initializes the transformer to be tested
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->transformer = new DataTransformer\JsonToAreasTransformer;
+    }
+    
+    /**
+     * Test Areas to Json transformation
+     * 
+     * @dataProvider jsonToAreasData
+     * @param type $expectedJson
+     * @param type $areas
+     */
+    public function testTransform($areas, $expectedJson)
+    {
+        $result = $this->transformer->transform($areas);
+        
+        $this->assertEquals($expectedJson, $result);
+        
+    }
+    
+    /**
+     * Test Json to Areas transformation
+     * 
+     * @dataProvider jsonToAreasData
+     * @param type $expectedJson
+     * @param type $areas
+     */
+    public function testReverseTransform($expectedAreas, $json)
+    {
+        $result = $this->transformer->reverseTransform($json);
+        $this->assertEquals($expectedAreas, $result);
+        
+    }
+    
+    /**
+     * Data provider
+     */
+    public function jsonToAreasData()
+    {
+        return array(
+            array(
+                array(
+                    array(
+                        'areaId' => 'SimpleArea',
+                        'boDirection' => null,
+                        'boPercent' => null,
+                        'classes' =>
+                        array(
+                            0 => 'class1',
+                            1 => 'class2',
+                        ),
+                        'subAreas' =>
+                        array(
+                        ),
+                        'blocks' =>
+                        array(
+                        ),
+                    ),
+                ),
+                '[{"areaId":"SimpleArea","boDirection":null,"boPercent":null,"classes":"class1,class2","blocks":[]}]'
+            )
+        );
+    }
+}
+
