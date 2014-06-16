17e4546b31d8babd04a7f62aa9f3552f04929600
Noël Gilain <noel.gilain@businessdecision.com>
2014-06-16 16:22:05 +0200

    Add ContentAttributesType unit test

---
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Test/Form/Type/ContentAttributesTypeTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * Business & Decision - Commercial License
+ *
+ * Copyright 2014 Business & Decision.
+ *
+ * All rights reserved. You CANNOT use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell this Software or any parts of this
+ * Software, without the written authorization of Business & Decision.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * See LICENSE.txt file for the full LICENSE text.
+ */
+
+namespace PHPOrchestra\CMSBundle\Test\Form\Type;
+
+use \PHPOrchestra\CMSBundle\Form\Type\ContentAttributesType;
+
+/**
+ * Description of ContentTypeTest
+ *
+ * @author Noël GILAIN <noel.gilain@businessdecision.com>
+ */
+class ContentAttributesTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $documentManager = $this->getMockBuilder('PHPOrchestra\\CMSBundle\\Document\\DocumentManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+      /*  $documentManager->expects($this->any())
+            ->method('createDocument')
+            ->will($this->returnValue(null));*/
+        
+        $this->contentAttributes = new ContentAttributesType($documentManager);
+    }
+
+    /**
+     * @dataProvider getOptions
+     * 
+     * @param array  $options
+     * @param int  $expectedAddCount
+     */
+    public function testBuildForm($options, $expectedAddCount)
+    {
+        $formBuilderMock =
+            $this->getMock('\\Symfony\\Component\\Form\\FormBuilderInterface');
+        
+        $formBuilderMock
+            ->expects($this->exactly($expectedAddCount))
+            ->method('add')
+            ->will($this->returnSelf());
+        
+        $this->contentAttributes->buildForm($formBuilderMock, $options);
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('contentAttributes', $this->contentAttributes->getName());
+    }
+    
+    public function getOptions()
+    {
+        $contentTypeWithNoField = $this->getMockBuilder('\\Model\\PHPOrchestraCMSBundle\\ContentType')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $dataWithNoField = (object) array('contentType' => $contentTypeWithNoField);
+        
+        $contentTypeWith3Fields = $this->getMockBuilder('\\Model\\PHPOrchestraCMSBundle\\ContentType')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $jsonFields = '[
+            {"fieldId": "field1", "defaultValue": "value1", "label": "label1", "symfonyType": "type1"},
+            {"fieldId": "field2", "defaultValue": "value2", "label": "label1", "symfonyType": "type2"},
+            {"fieldId": "field3", "defaultValue": "value3", "label": "label1", "symfonyType": "type3", "options": {"max_length": 0}}
+        ]';
+        $contentTypeWith3Fields
+            ->expects($this->any())
+            ->method('getFields')
+            ->will($this->returnValue($jsonFields));
+        
+        $dataWith3Fields = (object) array('contentType' => $contentTypeWith3Fields);
+        
+        return array(
+            array(array('data' =>  $dataWithNoField), 0),
+            array(array('data' =>  $dataWith3Fields), 3),
+        );
+    }
+    
+}
