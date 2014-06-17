f474c2c6a130a5fe72742078c094fa260b7526c7
Noël Gilain <noel.gilain@businessdecision.com>
2014-06-17 16:59:22 +0200

    Add ContentTypeTranformer unit test

---
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Test/Form/DataTransformer/ContentTypeTransformerTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace PHPOrchestra\CMSBundle\Test\Form\DataTransformer;
+
+use \PHPOrchestra\CMSBundle\Form\DataTransformer\ContentTypeTransformer;
+
+/**
+ * Description of ContentTypeTransformerTest
+ *
+ * @author Noël GILAIN <noel.gilain@businessdecision.com>
+ */
+class ContentTypeTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->transformer = new ContentTypeTransformer();
+    }
+    
+    /**
+     * @dataProvider getTransformData
+     * @param type $contentType
+     * @param type $expectedReturn
+     */
+    public function testTransform($contentType, $expectedReturn)
+    {
+        $result = $this->transformer->transform($contentType);
+        
+        $this->assertEquals($expectedReturn, $result);
+    }
+    
+    /**
+     * @dataProvider getReverseTransformData
+     * @param type $datas
+     * @param type $expectedReturn
+     */
+    public function testReverseTransform($datas, $expectedReturn)
+    {
+        $result = $this->transformer->reverseTransform($datas);
+        
+        $this->assertEquals($expectedReturn, $result);
+    }
+    
+    /**
+     * Data provider
+     */
+    public function getTransformData()
+    {
+        $fields = array(
+            'field1' => 'customField1',
+            'field2' => 'customField2',
+            'field3' => 'customField3',
+        );
+        
+        $result = new testContentType($fields);
+        $result->customField_field1 = 'customField1';
+        $result->customField_field2 = 'customField2';
+        $result->customField_field3 = 'customField3';
+        $result->customFieldsIndex = array('customField_field1', 'customField_field2', 'customField_field3');
+        
+        return array(
+            array(new testContentType(), new testContentType()),
+            array(new testContentType($fields), $result)
+        );
+    }
+    
+    /**
+     * Data provider
+     */
+    public function getReverseTransformData()
+    {
+        $fields = array(
+            'field1' => 'customField1',
+            'field2' => 'customField2',
+            'field3' => 'customField3',
+        );
+        
+        
+        $datas = new testContentType(
+            $fields,
+            array('customField1', 'customField2')
+        );
+        $datas->customField1 = null;
+        $datas->customField2 = 'value2';
+        
+        $result = clone $datas;
+        $result->setFields(json_encode(array('value2')));
+        
+        
+        $datasFull = new testContentType(
+            $fields,
+            array('customField1', 'customField2'),
+            'newField'
+        );
+        $datasFull->customField1 = 'value1';
+        $datasFull->customField2 = 'value2';
+        
+        $resultFull = clone $datasFull;
+        $resultFull->setFields(
+            json_encode(
+                array(
+                    'value1',
+                    'value2',
+                    (object) array(
+                        'fieldId' => '',
+                        'label' => '',
+                        'defaultValue' => '',
+                        'searchable' => false,
+                        'type' => 'newField',
+                        'symfonyType' => '',
+                        'options' => (object) array()
+                    )
+                )
+            )
+        );
+        
+        
+        return array(
+            array($datas, $result),
+            array($datasFull, $resultFull)
+        );
+    }
+}
+
+class testContentType
+{
+    public function __construct($fields = array(), $customFieldsIndex = array(), $new_field = '')
+    {
+        $this->setFields(json_encode($fields));
+        $this->customFieldsIndex = $customFieldsIndex;
+        $this->new_field = $new_field;
+    }
+    
+    public function getFields() {return $this->fields;}
+    public function setFields($fields) {$this->fields = $fields;}
+}
