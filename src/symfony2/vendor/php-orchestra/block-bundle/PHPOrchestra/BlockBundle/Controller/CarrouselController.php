a9fbd0ad723e879ebf207da621176b456e90fc65
ayman <ayman@ayman>
2014-06-26 11:30:29 +0200

    add CarrouselController

---
+++ src/symfony2/vendor/php-orchestra/block-bundle/PHPOrchestra/BlockBundle/Controller/CarrouselController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PHPOrchestra\BlockBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Description of CarousselController
+ * this controller allow to generate a caroussel
+ * @author Ayman AWAD <ayman.awad@businessdecision.com>
+ */
+
+class CarrouselController extends Controller
+{
+    function showAction($pictures = array(), $width , $height)
+    {
+    	return $this->render('PHPOrchestraBlockBundle:Carrousel:show.html.twig', array(
+    			'pictures' => $pictures,
+    	        'width'=> $width,
+    			'height'=> $height	
+    	));
+    }	
+    
+    /**
+     * Render the dialog form
+     *
+     * @param string $prefix
+     */
+    public function formAction($prefix)
+    {
+    	$form = $this->get('form.factory')
+    	->createNamedBuilder($prefix, 'form', null)
+    	->add(
+    			'pictures',
+    			'width',
+    			'height'
+    	)
+    	->getForm();
+    
+    	return $this->render(
+    			'PHPOrchestraBlockBundle:Carrousel:form.html.twig',
+    			array('form' => $form->createView())
+    	);
+    }
+    
+}
