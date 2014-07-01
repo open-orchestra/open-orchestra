d7b77a3e226e108bcb6f2c6def243e1592e08db9
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-07-01 11:38:06 +0200

    new nomenclature of blocks

---
+++ src/symfony2/vendor/php-orchestra/cms-bundle/PHPOrchestra/CMSBundle/Form/Type/Block/MenuType.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * Business & Decision - Commercial License
+*
+* Copyright 2014 Business & Decision.
+*
+* All rights reserved. You CANNOT use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell this Software or any parts of this
+* Software, without the written authorization of Business & Decision.
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* See LICENSE.txt file for the full LICENSE text.
+*/
+
+namespace PHPOrchestra\CMSBundle\Form\Type\Block;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Menu form builder
+ * 
+ * @author benjamin fouche <benjamin.fouche@businessdecision.com>
+ *
+ */
+class MenuType extends AbstractType
+{
+    /**
+     * (non-PHPdoc)
+     * @see src/symfony2/vendor/symfony/symfony/src/Symfony/Component/Form/Symfony
+     * \Component\Form.AbstractType::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('classMenu', 'textarea')
+            ->add('idMenu', 'text');
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see src/symfony2/vendor/symfony/symfony/src/Symfony/Component/Form/Symfony
+     * \Component\Form.FormTypeInterface::getName()
+     */
+    public function getName()
+    {
+        return 'menu';
+    }
+}
