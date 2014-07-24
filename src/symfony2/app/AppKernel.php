54c81eb037d4032d2e65c9024521bbaed759fafd
Nicolas Thal <nico.th4l@gmail.com>
2014-07-24 10:21:08 +0200

    suppress block bundle

--- src/symfony2/app/AppKernel.php
+++ src/symfony2/app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
 
             new PHPOrchestra\CMSBundle\PHPOrchestraCMSBundle(),
-            new PHPOrchestra\BlockBundle\PHPOrchestraBlockBundle(),
             new PHPOrchestra\ThemeBundle\PHPOrchestraThemeBundle(),
             new PHPOrchestra\ModelBundle\PHPOrchestraModelBundle(),
             new PHPOrchestraModel\MongoBundle\PHPOrchestraModelMongoBundle(),
