515f60f5049d5ab8041471b05ef624fe79ccddfe
Nicolas Thal <nico.th4l@gmail.com>
2014-11-12 17:18:07 +0100

    update vendor

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,6 +161,7 @@ module.exports = function(grunt) {
                      'web/built/phporchestrabackoffice/js/page/nodeView.js',
                      'web/built/phporchestrabackoffice/js/page/NodeVersionView.js',
                      'web/built/phporchestrabackoffice/js/page/NodeLanguageView.js',
+                     'web/built/phporchestrabackoffice/js/page/previewLinkView.js',
                      'web/built/phporchestrabackoffice/js/table/TableviewView.js',
                      'web/built/phporchestrabackoffice/js/table/TableviewCollectionView.js',
                      'web/built/phporchestrabackoffice/js/FullPageFormView.js',
