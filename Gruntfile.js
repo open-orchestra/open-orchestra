6f320d2e640852089ae7c3aa274d1aa1f181b025
Nicolas Thal <nico.th4l@gmail.com>
2015-01-13 12:26:28 +0100

    fix minified css issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,8 @@ module.exports = function(grunt) {
             
             libcss: {
                 src: [
-                      'bower_components/jquery-ui/themes/base/all.css',
+                      'bower_components/jquery-ui/themes/base/base.css',
+                      'bower_components/jquery-ui/themes/base/theme.css',
                       'bower_components/datatables/media/css/jquery.dataTables.css'
                 ],
                 dest: 'web/built/lib.css'
