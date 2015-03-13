3f90b46c4b0abcb2099184fcd284156ffb342ea4
Nicolas Thal <nico.th4l@gmail.com>
2015-03-13 11:18:43 +0100

    supress unused method

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -24,27 +24,6 @@ class FeatureContext extends MinkContext implements KernelAwareContext
     {
     }
 
-    /** Click on the element with the provided xpath query
-     *
-     * @When /^(?:|I )click on the element with xpath "([^"]*)"$/
-     */
-    public function iClickOnTheElementWithXPath($xpath)
-    {
-        $session = $this->getSession(); // get the mink session
-        $element = $session->getPage()->find(
-            'xpath',
-            $session->getSelectorsHandler()->selectorToXpath('xpath', $xpath)
-        ); // runs the actual query and returns the element
-
-        // errors must not pass silently
-        if (null === $element) {
-            throw new \InvalidArgumentException(sprintf('Could not evaluate XPath: "%s"', $xpath));
-        }
-
-        // ok, let's click on it
-        $element->click();
-    }
-
     /**
      * Click on the element with the provided Css Selector
      *
