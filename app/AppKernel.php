a798588576041e24c56697f1dda3820100774050
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-05-05 15:58:22 +0200

    add behat environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -51,7 +51,7 @@ class AppKernel extends Kernel
             new Solution\MongoAggregationBundle\SolutionMongoAggregationBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'behat'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
