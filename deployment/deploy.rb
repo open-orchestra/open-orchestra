cced7b9393eaf315f7926fa1ed2f9f0237f6f617
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-01-08 17:50:46 +0100

    bower compenents folder is move to vendor

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -5,6 +5,6 @@ set :keep_releases, 2
 
 set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --optimize-autoloader'
 set :linked_files, %w{app/config/parameters.yml}
-set :linked_dirs, %w{app/logs web/uploads vendor bower_components node_modules}
+set :linked_dirs, %w{app/logs web/uploads vendor node_modules}
 
 after 'deploy:finishing', 'deploy:cleanup'
