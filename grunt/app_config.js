55aaad24cf8bf1553e40bb7e0c26add8de44c939
Nicolas Thal <nico.th4l@gmail.com>
2016-01-19 18:14:42 +0100

    update file paths

--- grunt/app_config.js
+++ grunt/app_config.js
@@ -1,14 +1,14 @@
 module.exports = {
   GruntConfigBuilder:
-    '/var/www/openorchestra/vendor/open-orchestra/open-orchestra-cms-bundle/GruntTasks/GruntConfigBuilder.js',
+    './vendor/open-orchestra/open-orchestra-cms-bundle/GruntTasks/GruntConfigBuilder.js',
   tasksDir: [
-    '/var/www/openorchestra/grunt/tasks',
-    '/var/www/openorchestra/vendor/open-orchestra/open-orchestra-cms-bundle/GruntTasks',
-    '/var/www/openorchestra/vendor/open-orchestra/open-orchestra-media-admin-bundle/GruntTasks'
+    './grunt/tasks',
+    './vendor/open-orchestra/open-orchestra-cms-bundle/GruntTasks',
+    './vendor/open-orchestra/open-orchestra-media-admin-bundle/GruntTasks'
   ],
   targetsDir: [
-    '/var/www/openorchestra/grunt/targets',
-    '/var/www/openorchestra/vendor/open-orchestra/open-orchestra-cms-bundle/GruntTasks/Targets',
-    '/var/www/openorchestra/vendor/open-orchestra/open-orchestra-media-admin-bundle/GruntTasks/Targets'
+    './grunt/targets',
+    './vendor/open-orchestra/open-orchestra-cms-bundle/GruntTasks/Targets',
+    './vendor/open-orchestra/open-orchestra-media-admin-bundle/GruntTasks/Targets'
   ]
 };
