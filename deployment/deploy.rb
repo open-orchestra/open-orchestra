3a63f02d4e2ce0dfe1ac58de1b5bec2e1c93b8d5
itkg-ngilain <noel.gilain@businessdecision.com>
2016-02-22 17:39:55 +0100

    Add a capistrano task to push a fresh composer.lock

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -1,7 +1,7 @@
-
 set :format, :pretty
 set :log_level, :info
 set :keep_releases, 2
+set :ssh_options, { :forward_agent => true }
 
 set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --optimize-autoloader'
 set :linked_files, %w{app/config/parameters.yml}
--- deployment/stages/inte.rb
+++ deployment/stages/inte.rb
@@ -1,4 +1,7 @@
 server 'open_orchestra_backoffice_inte', roles: %w{web app db env}
 set :repo_url, 'git@github.com:open-orchestra/open-orchestra.git'
+set :update_dir, 'update-vendor-back-inte'
+set :git_project_dir, 'open-orchestra'
 set :deploy_to, '/var/www/backoffice-open-orchestra'
+set :branch, 'master'
 set :application, 'OpenOrchestra'
