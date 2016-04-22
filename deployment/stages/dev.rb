ebf83dfeb13a701f281a268cd300d7dcebf952a7
Ali Oumar <benomar01@gmail.com>
2016-04-22 15:43:00 +0200

    change deploy folder name

--- deployment/stages/dev.rb
+++ deployment/stages/dev.rb
@@ -2,6 +2,6 @@ server 'open_orchestra_backoffice_dev', roles: %w{web app db env}
 set :repo_url, 'git@github.com:open-orchestra/open-orchestra.git'
 set :update_dir, 'update-vendor-back-inte'
 set :git_project_dir, 'open-orchestra'
-set :deploy_to, '/var/www/openorchestra'
+set :deploy_to, '/var/www/open-orchestra-dev'
 set :branch, 'master'
 set :application, 'OpenOrchestra'
