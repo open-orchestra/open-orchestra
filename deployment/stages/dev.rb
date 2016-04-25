931b9b49e1671b2c357d78246c1b47c8445140f0
Ali Oumar <benomar01@gmail.com>
2016-04-25 13:33:13 +0200

    change command name

--- deployment/stages/dev.rb
+++ deployment/stages/dev.rb
@@ -1,6 +1,6 @@
 server 'open_orchestra_backoffice_dev', roles: %w{web app db env}
 set :repo_url, 'git@github.com:open-orchestra/open-orchestra.git'
-set :update_dir, 'update-vendor-back-inte'
+set :update_dir, 'update-vendor-back-dev'
 set :git_project_dir, 'open-orchestra'
 set :deploy_to, '/var/www/open-orchestra-dev'
 set :branch, 'master'
