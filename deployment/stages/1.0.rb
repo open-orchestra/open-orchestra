b9c2c5df1b89585f03638d4199bf4eaa3cf646bc
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-29 12:27:50 +0200

    add host 1.0 inte and provision

---
+++ deployment/stages/1.0.rb
@@ -0,0 +1,5 @@
+server 'open_orchestra_bo_inte_1-0', roles: %w{web app db env}
+set :repo_url, 'git@github.com:open-orchestra/open-orchestra.git'
+set :deploy_to, '/var/www/backoffice-open-orchestra'
+set :branch, '1.0'
+set :application, 'OpenOrchestraStable'
