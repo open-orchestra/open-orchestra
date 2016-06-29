33a24990e5f83c834a8303026c8efe545dfe4c0d
itkg-ngilain <noel.gilain@businessdecision.com>
2016-06-29 15:35:13 +0200

    Always enable Sensio/GeneratorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -62,12 +62,12 @@ class AppKernel extends Kernel
             new OpenOrchestra\ElasticaBundle\OpenOrchestraElasticaBundle(),
             new OpenOrchestra\ElasticaAdminBundle\OpenOrchestraElasticaAdminBundle(),
             new AntiMattr\Bundle\MongoDBMigrationsBundle\MongoDBMigrationsBundle(),
+            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
