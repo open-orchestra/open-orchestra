54aedc7d057974e27e899b72eb2baa93917d487a
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-07-25 10:18:26 +0200

    update functional for grant type password

--- src/OpenOrchestra/Tests/Functional/ApiBundle/Controller/AuthorizationControllersTest.php
+++ src/OpenOrchestra/Tests/Functional/ApiBundle/Controller/AuthorizationControllersTest.php
@@ -40,7 +40,13 @@ class AuthorizationControllersTest extends AbstractWebTestCase
         $this->assertSame('application/json', $this->client->getResponse()->headers->get('content-type'));
         $this->assertContains('token.blocked', $this->client->getResponse()->getContent());
 
-        $this->client->request('GET', '/oauth/access_token?grant_type=password&username=admin&password=admin', array(), array(), array('PHP_AUTH_USER' => 'test_key', 'PHP_AUTH_PW' => 'test_secret'));
+        $headers = array(
+            'PHP_AUTH_USER' => 'test_key',
+            'PHP_AUTH_PW' => 'test_secret',
+            'HTTP_username' => 'admin',
+            'HTTP_password' => 'admin',
+        );
+        $this->client->request('GET', '/oauth/access_token?grant_type=password', array(), array(), $headers);
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertSame('application/json', $this->client->getResponse()->headers->get('content-type'));
         $tokenReponse = json_decode($this->client->getResponse()->getContent(), true);
