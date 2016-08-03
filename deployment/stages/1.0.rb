fc294442f56634f0c184dd30962d5b85acb9c424
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-08-03 14:28:26 +0200

    Update deploy config (#1008)
    * update deploy config
    
    * update dist
    
    * update vendor

--- deployment/stages/1.0.rb
+++ deployment/stages/1.0.rb
@@ -1,6 +1,6 @@
 server 'open_orchestra_bo_inte_1-0', roles: %w{web app db env}
-set :repo_url, 'git@github.com:open-orchestra/open-orchestra.git'
-set :deploy_to, '/var/www/backoffice-open-orchestra'
+set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
+set :deploy_to, '/home/wwwroot/openorchestra/back'
 set :branch, '1.0'
 set :application, 'OpenOrchestraStable'
 set :update_dir, 'update-vendor-back-inte'
