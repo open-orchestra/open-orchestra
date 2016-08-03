5c0c444b2920e97663aeaaa5a75747c9ceb38424
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-08-03 10:53:30 +0200

    update config deploy

--- deployment/stages/1.1.rb
+++ deployment/stages/1.1.rb
@@ -2,6 +2,6 @@ server 'open_orchestra_bo_inte_1-1', roles: %w{web app db env}
 set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
 set :update_dir, 'update-vendor-back-inte'
 set :git_project_dir, 'open-orchestra'
-set :deploy_to, '/home/wwwroot/openorchestra/backoffice-open-orchestra'
+set :deploy_to, '/home/wwwroot/openorchestra/back'
 set :branch, '1.1'
 set :application, 'OpenOrchestra'
