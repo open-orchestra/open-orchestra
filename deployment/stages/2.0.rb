8944b90a47fab6de14bc2da23cac778dae4b2902
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-10-07 16:35:44 +0200

    update deploy 2.0

--- deployment/stages/2.0.rb
+++ deployment/stages/2.0.rb
@@ -1,4 +1,4 @@
-server 'open_orchestra_bo_inte_1-2', roles: %w{web app db env}
+server 'open_orchestra_bo_inte_2-0', roles: %w{web app db env}
 set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
 set :update_dir, 'update-vendor-back-inte'
 set :git_project_dir, 'open-orchestra'
