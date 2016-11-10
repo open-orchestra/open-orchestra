338294226bf467299958eea09533b2ea60df5c94
itkg-ngilain <noel.gilain@businessdecision.com>
2016-11-10 12:13:15 +0100

    Fix broken functional tests

--- src/OpenOrchestra/Tests/Functional/ApiBundle/Controller/NodeControllerAccessTest.php
+++ src/OpenOrchestra/Tests/Functional/ApiBundle/Controller/NodeControllerAccessTest.php
@@ -2,11 +2,8 @@
 
 namespace OpenOrchestra\FunctionalTests\ApiBundle\Controller;
 
-use OpenOrchestra\Backoffice\NavigationPanel\Strategies\TreeNodesPanelStrategy;
-use OpenOrchestra\Backoffice\Model\ModelGroupRoleInterface;
 use OpenOrchestra\Backoffice\Repository\GroupRepositoryInterface;
 use OpenOrchestra\FunctionalTests\Utils\AbstractAuthenticatedTest;
-use OpenOrchestra\ModelInterface\Model\NodeInterface;
 
 /**
  * Class NodeControllerAccessTest
@@ -40,40 +37,9 @@ class NodeControllerAccessTest extends AbstractAuthenticatedTest
         $nodeId = 'fixture_page_community';
         $groupName = 'Demo group';
 
-        $this->updateModelGroupRole($groupName, $nodeId, ModelGroupRoleInterface::ACCESS_DENIED, TreeNodesPanelStrategy::ROLE_ACCESS_UPDATE_NODE);
-
         $this->client->request('POST', '/api/node/' . $nodeId . '/new-version?language=fr');
 //        $this->assertSame(403, $this->client->getResponse()->getStatusCode());
         $this->assertContains('open_orchestra_api.node.new_version_not_granted', $this->client->getResponse()->getContent());
-
-        $this->updateModelGroupRole($groupName, $nodeId, ModelGroupRoleInterface::ACCESS_INHERIT, TreeNodesPanelStrategy::ROLE_ACCESS_UPDATE_NODE);
     }
 
-    /**
-     * @param string $groupName
-     * @param string $modelId
-     * @param string $accessType
-     * @param string $role
-     */
-    protected function updateModelGroupRole($groupName, $modelId, $accessType, $role)
-    {
-        $group = $this->groupRepository->findOneBy(array('name' => $groupName));
-        $groupId = $group->getId();
-        $this->client->request(
-            'POST',
-            '/api/group/' . $groupId,
-            array(),
-            array(),
-            array(),
-            json_encode(array('model_roles' => array(
-                array(
-                    'model_id' => $modelId,
-                    'type' => NodeInterface::GROUP_ROLE_TYPE,
-                    'access_type' => $accessType,
-                    'name' => $role
-                )
-            )))
-        );
-        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
-    }
 }
