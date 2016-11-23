cee38094ab0201c216b4135035c5a604a4188c45
itkg-ngilain <noel.gilain@businessdecision.com>
2016-11-23 17:41:39 +0100

    Fix tests

--- src/OpenOrchestra/Tests/Functional/ModelBundle/Repository/NodeRepositoryTest.php
+++ src/OpenOrchestra/Tests/Functional/ModelBundle/Repository/NodeRepositoryTest.php
@@ -695,7 +695,7 @@ class NodeRepositoryTest extends AbstractKernelTestCase
         $this->assertCount(5, $nodeRootTree['child']);
         $this->assertSame('root', $nodeRoot['nodeId']);
         $childrenRoot = $nodeRootTree['child'];
-        $orderNodeId = array('fixture_page_community', 'fixture_page_contact', 'fixture_page_news', 'fixture_page_legal_mentions', 'fixture_auto_unpublish');
+        $orderNodeId = array('fixture_page_community', 'fixture_page_news', 'fixture_page_contact', 'fixture_page_legal_mentions', 'fixture_auto_unpublish');
         foreach ($childrenRoot as $index => $child) {
             $this->assertCount(0, $child['child']);
             $this->assertSame($orderNodeId[$index], $child['node']['nodeId']);
