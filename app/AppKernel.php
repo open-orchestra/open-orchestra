52aa5e96e1c020ddccd35cf3d8654e9279ef6aa7
itkg-ngilain <noel.gilain@businessdecision.com>
2016-11-29 11:57:21 +0100

    Move WorkflowFunctionModelBundle to ModelBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,7 +40,6 @@ class AppKernel extends Kernel
             new OpenOrchestra\MongoBundle\OpenOrchestraMongoBundle(),
             new OpenOrchestra\MediaBundle\OpenOrchestraMediaBundle(),
             new OpenOrchestra\MediaModelBundle\OpenOrchestraMediaModelBundle(),
-            new OpenOrchestra\WorkflowFunctionModelBundle\OpenOrchestraWorkflowFunctionModelBundle(),
             new OpenOrchestra\WorkflowFunctionBundle\OpenOrchestraWorkflowFunctionBundle(),
 
             new OpenOrchestra\ApiBundle\OpenOrchestraApiBundle(),
