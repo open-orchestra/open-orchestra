1740f2296743da1a70d6457bca9eeb0207b51ccf
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-12-19 11:21:44 +0100

    add paginate test functional

--- src/OpenOrchestra/Tests/Functional/ModelBundle/Repository/WorkflowProfileRepositoryTest.php
+++ src/OpenOrchestra/Tests/Functional/ModelBundle/Repository/WorkflowProfileRepositoryTest.php
@@ -3,6 +3,8 @@
 namespace OpenOrchestra\FunctionalTests\ModelBundle\Repository;
 
 use OpenOrchestra\BaseBundle\Tests\AbstractTest\AbstractKernelTestCase;
+use OpenOrchestra\ModelInterface\Repository\WorkflowProfileRepositoryInterface;
+use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
 
 /**
  * Class WorkflowProfileRepositoryTest
@@ -57,6 +59,75 @@ class WorkflowProfileRepositoryTest extends AbstractKernelTestCase
         return array(
             array('draft', 'toTranslate', false),
             array('draft', 'published'  , true),
+        );
+    }
+
+    /**
+     * @param PaginateFinderConfiguration  $configuration
+     * @param int                          $count
+     *
+     * @dataProvider providePaginateAndSearch
+     */
+    public function testFindForPaginate(PaginateFinderConfiguration $configuration, $count)
+    {
+        $workflowProfiles = $this->repository->findForPaginate($configuration);
+        $this->assertCount($count, $workflowProfiles);
+    }
+
+    /**
+     * @return array
+     */
+    public function providePaginateAndSearch()
+    {
+        $configurationAll = PaginateFinderConfiguration::generateFromVariable(array(), 0, 100, array());
+        $configurationLimit = PaginateFinderConfiguration::generateFromVariable(array(), 0, 1, array());
+        $configurationSearch = PaginateFinderConfiguration::generateFromVariable(array(), 0, 100, array(), array('label' => 'Contributor'));
+        $configurationAllOrder = PaginateFinderConfiguration::generateFromVariable(array('label' => 'desc'), 0, 100, array());
+
+        return array(
+            'all' => array($configurationAll, 2),
+            'limit' => array($configurationLimit, 1),
+            'search' => array($configurationSearch, 1),
+            'order' => array($configurationAllOrder, 2),
+        );
+    }
+
+    /**
+     * test count all workflow profile
+     */
+    public function testCount()
+    {
+        $workflowProfiles = $this->repository->count();
+        $this->assertEquals(2, $workflowProfiles);
+    }
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     * @param int                         $count
+     *
+     * @dataProvider provideCountWithFilter
+     */
+    public function testCountWithFilter($configuration, $count)
+    {
+        $workflowProfiles = $this->repository->countWithFilter($configuration);
+        $this->assertEquals($count, $workflowProfiles);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideCountWithFilter()
+    {
+        $configurationAll = PaginateFinderConfiguration::generateFromVariable(array(), 0, 100, array());
+        $configurationLimit = PaginateFinderConfiguration::generateFromVariable(array(), 0, 1, array());
+        $configurationSearch = PaginateFinderConfiguration::generateFromVariable(array(), 0, 100, array(), array('label' => 'Contributor'));
+        $configurationAllOrder = PaginateFinderConfiguration::generateFromVariable(array('label' => 'desc'), 0, 100, array());
+
+        return array(
+            'all' => array($configurationAll, 2),
+            'limit' => array($configurationLimit, 2),
+            'search' => array($configurationSearch, 1),
+            'order' => array($configurationAllOrder, 2),
         );
     }
 }
