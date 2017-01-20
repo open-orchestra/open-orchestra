a487eb6067641219084dec93e20367375e079419
Noël GILAIN <noel.gilain@businessdecision.com>
2017-01-20 11:02:22 +0100

    2 0 logs (#1130)
    * include logBundlle in the client application
    
    * Add LogRepository functional test
    
    * Update composer

--- grunt/targets/application.config.js
+++ grunt/targets/application.config.js
@@ -4,6 +4,7 @@ module.exports = {
             'openorchestrabackoffice',
             'openorchestrauseradmin',
             'openorchestragroup',
+            'openorchestralog',
             'openorchestraworkflowadmin',
             'openorchestramediaadmin'
         ],
