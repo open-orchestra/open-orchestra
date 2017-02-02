b6869419a942de4e3ce555fcede996d7592fe581
itkg-ngilain <noel.gilain@businessdecision.com>
2017-02-02 18:16:55 +0100

    Add folderRepository tests

--- src/OpenOrchestra/Tests/Functional/MediaAdminBundle/Repository/FolderRepositoryTest.php
+++ src/OpenOrchestra/Tests/Functional/MediaAdminBundle/Repository/FolderRepositoryTest.php
@@ -52,4 +52,32 @@ class FolderRepositoryTest extends AbstractKernelTestCase
             array('3', 2),
         );
     }
+
+    /**
+     * test findFolderTree
+     */
+    public function testFindFolderTree()
+    {
+        $tree = $this->repository->findFolderTree('2');
+
+        $this->assertTrue(is_array($tree));
+        $this->assertSame(2, count($tree));
+        $this->assertTree($tree[0], 2);
+        $this->assertTree($tree[1], 1);
+        $this->assertTree($tree[0]['children'][0], 1);
+        $this->assertTree($tree[0]['children'][1], 0);
+        $this->assertTree($tree[1]['children'][0], 0);
+    }
+
+    /**
+     * @param mixed $tree
+     * @param int   $childrenCount
+     */
+    protected function assertTree($tree, $childrenCount)
+    {
+        $this->assertTrue(is_array($tree));
+        $this->assertArrayHasKey('folder', $tree);
+        $this->assertArrayHasKey('children', $tree);
+        $this->assertSame($childrenCount, count($tree['children']));
+    }
 }
