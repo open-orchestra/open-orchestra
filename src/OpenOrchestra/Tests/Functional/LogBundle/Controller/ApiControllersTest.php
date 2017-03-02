7eed6fe4a019c841f1082fdeccfa0415481f17c5
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-03-02 14:24:09 +0100

    Remove useless code (#1155)
    * remove jmeter
    
    * fix functional test

--- src/OpenOrchestra/Tests/Functional/LogBundle/Controller/ApiControllersTest.php
+++ src/OpenOrchestra/Tests/Functional/LogBundle/Controller/ApiControllersTest.php
@@ -18,8 +18,6 @@ class ApiControllersTest extends AbstractAuthenticatedTest
      */
     public function testApi($url)
     {
-        $this->markTestSkipped('To reactivate when API roles will be implemented');
-
         $this->client->request('GET', $url . '?access_token=' . $this->getAccessToken());
 
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
