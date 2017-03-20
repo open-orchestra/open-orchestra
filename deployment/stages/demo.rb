aa47d76540171e7d6cec351104f3263ec37351f3
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-03-20 15:28:37 +0100

    add demo stages capistrano

---
+++ deployment/stages/demo.rb
@@ -0,0 +1,7 @@
+server 'open_orchestra_bo_inte_demo', roles: %w{web app db env}
+set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
+set :update_dir, 'update-vendor-back-inte'
+set :git_project_dir, 'open-orchestra'
+set :deploy_to, '/home/wwwroot/openorchestra/back'
+set :branch, 'master'
+set :application, 'OpenOrchestra'
