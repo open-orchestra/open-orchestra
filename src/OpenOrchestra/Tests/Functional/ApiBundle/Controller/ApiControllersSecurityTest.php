40683b487ea7a3cc161a5d412379febb5266ccdb
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-03-28 10:23:34 +0200

    fix functional test (#1172)

--- src/OpenOrchestra/Tests/Functional/ApiBundle/Controller/ApiControllersSecurityTest.php
+++ src/OpenOrchestra/Tests/Functional/ApiBundle/Controller/ApiControllersSecurityTest.php
@@ -24,6 +24,7 @@ class ApiControllersSecurityTest extends AbstractAuthenticatedTest
     {
         $this->client->request($method, $url . '?access_token=' . $this->getAccessToken());
         $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+
     }
 
     /**
@@ -43,16 +44,13 @@ class ApiControllersSecurityTest extends AbstractAuthenticatedTest
             20 => array('/api/group/list'),
             21 => array('/api/group/user/list'),
             22 => array('/api/group/duplicate', "POST"),
-            23 => array('/api/content/list/lorem_ipsum/2/fr'),
             24 => array('/api/content/duplicate', "POST"),
             25 => array('/api/content/delete/lorem_ipsum', "DELETE"),
-            29 => array('/api/content/list-version/lorem_ipsum/fr'),
             30 => array('/api/redirection'),
             32 => array('/api/redirection/node/2/root/fr'),
             33 => array('/api/status'),
             35 => array('/api/content/new-version/r5_3_portes/fr/2', 'POST'),
-            36 => array('/api/node/new-version/root/fr/1', 'POST'),
-            37 => array('/api/content/show/lorem_ipsum/fr'),
+            36 => array('/api/node/new-version/root/fr/1', 'POST')
         );
     }
 }
