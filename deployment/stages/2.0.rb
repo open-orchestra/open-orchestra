287eee98db297e2df08d0a1f12cedd36c3c9f249
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-03-30 16:23:20 +0200

    clean capistrano deploy

--- deployment/stages/2.0.rb
+++ deployment/stages/2.0.rb
@@ -1,7 +1,5 @@
 server 'open_orchestra_bo_inte_2-0', roles: %w{web app db env}
 set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
-set :update_dir, 'update-vendor-back-inte'
-set :git_project_dir, 'open-orchestra'
 set :deploy_to, '/home/wwwroot/openorchestra/back'
 set :branch, 'master'
 set :application, 'OpenOrchestra'
