c3eba24a71bada2fd9b635b4587c1ba53629539c
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-04-06 11:06:17 +0200

    move conf capistrano (#1185)

--- deployment/stages/2.0.rb
+++
@@ -1,5 +0,0 @@
-server 'open_orchestra_bo_inte_2-0', roles: %w{web app db env}
-set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
-set :deploy_to, '/home/wwwroot/openorchestra/back'
-set :branch, 'master'
-set :application, 'OpenOrchestra'
--- deployment/stages/demo.rb
+++
@@ -1,5 +0,0 @@
-server 'open_orchestra_bo_inte_demo', roles: %w{web app db env}
-set :repo_url, 'https://github.com/open-orchestra/open-orchestra.git'
-set :deploy_to, '/home/wwwroot/openorchestra/back'
-set :branch, 'master'
-set :application, 'OpenOrchestra'
