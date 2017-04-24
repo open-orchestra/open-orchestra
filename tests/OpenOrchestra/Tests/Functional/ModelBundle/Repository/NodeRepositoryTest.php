54511c45e27bb8715677b2b9f9ff43d2709067d7
itkg-nanne <nicolas.anne@businessdecision.com>
2017-04-24 15:52:37 +0200

    restore node under father

--- tests/OpenOrchestra/Tests/Functional/ModelBundle/Repository/NodeRepositoryTest.php
+++ tests/OpenOrchestra/Tests/Functional/ModelBundle/Repository/NodeRepositoryTest.php
@@ -954,6 +954,29 @@ class NodeRepositoryTest extends AbstractKernelTestCase
     }
 
     /**
+     * @param string $nodeId
+     * @param string $siteId
+     * @param int    $count
+     *
+     * @dataProvider provideCountById
+     */
+    public function testCountById($nodeId, $siteId, $count)
+    {
+        $this->assertEquals($count, $this->repository->countByParentId($nodeId, $siteId));
+    }
+
+    /**
+     * @return array
+     */
+    public function provideCountById()
+    {
+        return array(
+            array('fixture_page_contact', '2', 0),
+            array('root', '2', 15)
+        );
+    }
+
+    /**
      * Test update embedded status
      */
     public function testUpdateEmbeddedStatus()
